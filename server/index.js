ebff893db8fe2f3b212013a240ea43b484b85f3d
Zohaib <zohaib.hassan@mail.mcgill.ca>
2018-07-06 21:22:11 +0000

    Final mongodb implementation

--- server/index.js
+++ server/index.js
@@ -7,9 +7,8 @@ const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.use(bodyParser.json());
 
 // The in-memory database of tweets. It's a basic object with an array in it.
 const MongoClient = require('mongodb').MongoClient;
--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -1,7 +1,5 @@
 "use strict";
 
-// Simulates the kind of delay we see with network or filesystem operations
-const simulateDelay = require("./util/simulate-delay");
 
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
@@ -10,11 +8,8 @@ module.exports = function makeDataHelpers(db) {
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
       
-        db.collection('tweets').find().toArray((err, tweets)=>{
-          if (err) throw err;
-          callback(null, tweets);
-
-        });      
+       db.collection('tweets').save(newTweet)
+          callback(null, true);   
     },
 
     // Get all tweets in `db`, sorted by newest first
